fc881f44cb0b05d054cf5e5602c97621b6b3f2e8
catborn <itnoz@yandex.ru>
2024-09-28 14:20:25 +0300

    add title

---
+++ src/components/data.js
@@ -0,0 +1,28 @@
+const data = [
+  {
+    id: 1,
+    title: "Безлимитный 300",
+    price: 300,
+    speed: 10,
+  },
+  {
+    id: 2,
+    title: "Безлимитный 450",
+    price: 450,
+    speed: 50,
+  },
+  {
+    id: 3,
+    title: "Безлимитный 550",
+    price: 550,
+    speed: 100,
+  },
+  {
+    id: 4,
+    title: "Безлимитный 1000",
+    price: 1000,
+    speed: 200,
+  },
+];
+
+export default data;
