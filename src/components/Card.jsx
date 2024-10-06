c28c2b22527b8b23546957f048c17b2fba4978d6
catborn <itnoz@yandex.ru>
2024-10-06 15:10:22 +0300

    add height 550

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,8 +1,14 @@
+import React from "react";
 import styles from "./Сard.module.css";
 
 function Card(props) {
+  const cardStyle = {
+    backgroundColor: props.color,
+    height: props.price === 550 ? "360px" : "330px",
+    width: props.price === 550 ? "290px" : "250px",
+  };
   return (
-    <div className={styles.card} style={{ backgroundColor: props.color }}>
+    <div className={styles.card} style={cardStyle}>
       <h2>{props.title}</h2>
       <p>руб</p>
       <p className={styles.price}>{props.price}</p>
