f7bfd152e6b80a32b71a3f427ac4ff77b4c2d52d
catborn <itnoz@yandex.ru>
2024-10-06 19:18:35 +0300

    add tariff select

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,14 +1,23 @@
 import React from "react";
+import { useState } from "react";
 import styles from "./Сard.module.css";
 
 function Card(props) {
+  const [isSelected, setIsSelected] = useState(false);
+
   const cardStyle = {
     backgroundColor: props.color,
-    height: props.price === 550 ? "360px" : "330px",
+    height: props.price === 550 ? "380px" : "360px",
     width: props.price === 550 ? "290px" : "250px",
   };
+
+  const handleClick = () => {
+    setIsSelected(!isSelected);
+  };
+
   return (
-    <div className={styles.card} style={cardStyle}>
+    <div className={styles.card} style={cardStyle} onClick={handleClick}>
+      {isSelected && <div className={styles.selected}>Тариф выбран</div>}
       <h2>{props.title}</h2>
       <p>руб</p>
       <p className={styles.price}>{props.price}</p>
